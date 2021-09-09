02a4f66fcfd22d93bd2676c23736281926c69606
Joao <joaocfranco81@gmail.com>
2021-09-08 22:56:05 -0300

    creando directorios

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// let nombre = "coderhouse";
+
+// console.log(nombre);
+
+// const user = { name: "joao", lastname:"franco"};
+
+// console.log(user);
+
+const mostrarLista = { "verde", "rojo", "azul"};
+
+console.log(mostrarLista);
